116db75ce1e347e722d4cbb901c2c02bce0d8bb1
Code Sandbox <hello@codesandbox.io>
2021-12-21 11:15:41 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.ff9239804f85f094c01976398bea40e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6cb02ae9b1ae6888d91d9b556beb8bc",
+    "url": "https://iceymann18777.github.io/csb-3ud0y/index.html"
+  },
+  {
+    "revision": "9e83c1032ab0cc5b6a46",
+    "url": "https://iceymann18777.github.io/csb-3ud0y/static/css/2.66398b45.chunk.css"
+  },
+  {
+    "revision": "0a11bc968b84f8e2a724",
+    "url": "https://iceymann18777.github.io/csb-3ud0y/static/css/main.eaab0393.chunk.css"
+  },
+  {
+    "revision": "9e83c1032ab0cc5b6a46",
+    "url": "https://iceymann18777.github.io/csb-3ud0y/static/js/2.2583d1b0.chunk.js"
+  },
+  {
+    "revision": "f1a5cb264c09c7bb73876fd1cbe974a6",
+    "url": "https://iceymann18777.github.io/csb-3ud0y/static/js/2.2583d1b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a11bc968b84f8e2a724",
+    "url": "https://iceymann18777.github.io/csb-3ud0y/static/js/main.d2e52bbd.chunk.js"
+  },
+  {
+    "revision": "ea0a390d3a258fa7f3bc",
+    "url": "https://iceymann18777.github.io/csb-3ud0y/static/js/runtime-main.bc01a20e.js"
+  },
+  {
+    "revision": "4173ecfedf49063b4a20b48ff665ef5e",
+    "url": "https://iceymann18777.github.io/csb-3ud0y/static/media/home-bg-1.4173ecfe.jpg"
+  }
+]);
